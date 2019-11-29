4d47271b24af77d9f40033a0d48ac57c91632797
joanna.senczuk <joanna.senczuk@ocado.com>
2019-11-29 11:47:42 +0000

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
       <h1>React App</h1>
     </div>
   );
+
+  // this is equivalent of the above. createElement takes (type, props, any number of children...)
+  // if children are html tags then they have to be createElement()
+  // this is what the above gets compiled to eventually
+  // return React.createElement('div', {className:'App'},
+  //     React.createElement('h1', null, 'React App'))
 }
 
 export default App;
