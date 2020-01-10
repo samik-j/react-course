b9ac3ada911a894a9f69db18e199b9579230f3b4
joanna.senczuk <joanna.senczuk@ocado.com>
2020-01-10 13:07:36 +0000

    Adding Two Way Binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const person = ({name, age, children, click}) => {
+const person = ({name, age, color, children, click, change}) => {
 	return (
 		<div>
-			<p onClick={click}>{name} here and I am {age} years old</p>
+			<p onClick={click}>{name} here and I am {age} years old, I am {color}</p>
+			<input type="text" onChange={change} value={color}/>
 			{/*if it's (props) then this is props.children, children is a reserved keyword*/}
 			<p>{children}</p>
 		</div>
