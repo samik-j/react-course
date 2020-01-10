fb1abbe3e59c61af4a8f65fb8c2fbd5aaef6a32a
joanna.senczuk <joanna.senczuk@ocado.com>
2020-01-10 11:38:22 +0000

    Use old state for one entity

--- src/AppFunctionalComponentWithHooks.js
+++ src/AppFunctionalComponentWithHooks.js
@@ -7,11 +7,12 @@ const AppFunctionalComponentWithHooks = props => {
     // this is destructuring so i can specify the names for the two things that get returned
     const [ entitiesState, setEntitiesState ] = useState({
         entities: [
-            {name: "llama", movement: "jumping", state: "I am jumping happily"}
+            {name: "llama", movement: "jumping", state: "I am jumping happily"},
+            {name: "other llama", movement: "jumping", state: "I am jumping happily"}
         ]
     });
 
-    // if otherStateThingy was in entitiesState then it would be erased on setEntitiesState that has only entities inside
+    // if otherStateThingy was in entitiesState then it would be erased on setEntitiesState (if it doesn't set it) that has only entities inside
     // so one state per "property"
     const [ otherStateThingy, setOtherStateThingy ] = useState({
         otherStateThingy: "other thingy"
@@ -24,6 +25,7 @@ const AppFunctionalComponentWithHooks = props => {
         setEntitiesState({
             entities: [
                 {name: 'llama', movement: 'jumping', state: newState},
+                {name: entitiesState.entities[1].name, movement: 'jumping', state: entitiesState.entities[1].state}
             ]
         })
     };
@@ -34,6 +36,7 @@ const AppFunctionalComponentWithHooks = props => {
             <button onClick={switchEntityHandler}>Trip or help</button>
             <Person name="llama">This is inside</Person>
             <Person name={entitiesState.entities[0].name}>{entitiesState.entities[0].state}</Person>
+            <Person name={entitiesState.entities[1].name}>{entitiesState.entities[1].state}</Person>
             <p>{otherStateThingy.otherStateThingy}</p>
         </div>
     );
